0bdf634e45918388ff12424aeea44e2e66a4e52c
Samito-05 <procoppesam@gmail.com>
2024-05-01 14:33:01 +0200

    Update Programme.c

--- Programme.c
+++ Programme.c
@@ -1,20 +1,2 @@
 #include <stdio.h>
 
-int coefbi(int n,int p){
-  if(p==0){
-    return 1;
-  }
-  if(p==n){
-    return 1;
-  }
-  else{
-    return coefbi(n-1,p-1) + coefbi(n-1,p);
-  }
-}
-
-int main(){
-  printf("%d\n",coefbi(10,3));
-  printf("ros");
-  return 0;
-}
-//caca prout
