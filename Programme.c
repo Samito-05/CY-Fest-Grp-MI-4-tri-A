16ce4f475df9c9e2f230357bdbc5ea90f973933f
Utilisateur <landiergab@cy-tech.fr>
2024-05-01 14:02:26 +0200

    modification test

--- Programme.c
+++ Programme.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
+
+int coefbi(int n,int p){
+  if(p==0){
+    return 1;
+  }
+  if(p==n){
+    return 1;
+  }
+  else{
+    return coefbi(n-1,p-1) + coefbi(n-1,p);
+  }
+}
 
 int main(){
-
-    printf("Hello world !");
-
-    return 0;
-
-    }
+  printf("%d",coefbi(10,3));
+  return 0;
+}
